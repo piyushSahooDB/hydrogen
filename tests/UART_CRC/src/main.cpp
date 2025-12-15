bb8ed4291305d4723a0f74e11736a2788262485c
Azad Roy <jovianarsenic@gmail.com>
2025-12-15 23:20:14 +0530

    bms frame structure somewhat architechted

--- tests/UART_CRC/src/main.cpp
+++ tests/UART_CRC/src/main.cpp
@@ -2,6 +2,24 @@
 #include <Arduino.h>
 #include <string.h>
 
+/*
+    Sending frame structure:
+    - 8 bits sof
+    - 16 bits BMS Current
+    - 16 bits BMS output voltage
+    - 16 bits Total Battery Voltage
+    - 16 bits Temp
+    - 8 bits ERROR (say bms turned off or smth)
+    - 8 bits CRC
+    SOF is AA
+
+    Receiving frame structure:
+    COMMAND : TURN OFF THRUSTER POWER (0x11)
+    COMMAND : TURN ON THRUSTER POWER  (0x22)
+    COMMAND : 
+
+*/
+
 String receivedMessage = "";
 
 void setup() {
