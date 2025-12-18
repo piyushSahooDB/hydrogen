fcb9280977a19bac3cd79cfabdc6cf71be35aadd
ChaturVasireddy <chaturvasireddy@gmail.com>
2025-12-18 16:27:18 +0530

    pooltest masteresp32

---
+++ pooltest/masteresp32/src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+uint16_t escframegen(uint16_t throttle) {
+  throttle &= 0x7FF;
+  uint16_t packet = (throttle << 1) | 0;
+  unsigned int crc = (packet ^ (packet >> 4) ^ (packet >> 8)) & 0x0F;
+  uint16_t escframe = (packet << 4) | crc;
+  return escframe;
+}
+
+#define TXPIN   17
+#define RXPIN   16
+#define BAUDRATE  115200
+
+HardwareSerial picolink(2);
+
+void setup() {
+  Serial.begin(115200);
+  picolink.begin(BAUDRATE, SERIAL_8N1, RXPIN, TXPIN);
+}
+
+void send_escframe(uint16_t escframe) {
+  uint8_t hi = (escframe >> 8) & 0xFF;
+  uint8_t lo = escframe & 0xFF;
+  picolink.write(hi);
+  picolink.write(lo);
+}
+
+void loop() {
+  uint16_t throttle = 100;
+  send_escframe(escframegen(throttle));
+  delay(1000);
+}
