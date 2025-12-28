b2af7584f3dbef3941ca2d12202f2df373fecef2
ChaturVasireddy <chaturvasireddy@gmail.com>
2025-12-28 13:33:31 +0530

    cleaned up pico esc code. uses classes now

---
+++ tests/thrusters_system/esp32_master/src/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+//PICO uses Serial1
+#define PICO_TXPIN   17
+#define PICO_RXPIN   16
+#define PICO_BAUDRATE  115200
---
+++ tests/thrusters_system/esp32_master/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include "pico_esc.h"
+
+void setup() {
+  pico_esc::pico_uart_init();
+  pico_esc::arm_thrusters();
+}
+
+void loop() {
+  uint16_t throttle[5] = { 1347,1347,1347,1347,1347 };
+  for (int i = 0;i < 5;i++) {
+    Serial1.write(0b00010000 | i);
+    pico_esc::send_escframe(throttle[i]);
+  }
+  delayMicroseconds(800);
+}
