85782c724dbcb39536fac46652e05b5c52271ae3
ChaturVasireddy <chaturvasireddy@gmail.com>
2026-01-14 18:37:57 +0530

    bidir dshot data decoding 1

--- tests/protocols/bidir_dshot/dshot.c
+++ tests/protocols/bidir_dshot/dshot.c
@@ -5,6 +5,28 @@
 
 #include "dshot.pio.h"
 
+uint8_t gcr_decode(uint8_t code5)
+{
+    switch (code5) {
+    case 0x19: return 0x0;
+    case 0x1B: return 0x1;
+    case 0x12: return 0x2;
+    case 0x13: return 0x3;
+    case 0x1D: return 0x4;
+    case 0x15: return 0x5;
+    case 0x16: return 0x6;
+    case 0x17: return 0x7;
+    case 0x1A: return 0x8;
+    case 0x09: return 0x9;
+    case 0x0A: return 0xA;
+    case 0x0B: return 0xB;
+    case 0x1E: return 0xC;
+    case 0x0D: return 0xD;
+    case 0x0E: return 0xE;
+    case 0x0F: return 0xF;
+    default:   return 0xFF;
+    }
+}
 int main() {
 
     stdio_init_all();
@@ -32,9 +54,26 @@ int main() {
     }
     while (true) {
         pio_sm_put_blocking(pio, sm, (uint32_t)0x126A << 16);
-        uint32_t v = pio_sm_get_blocking(pio, sm);
+        uint32_t value = pio_sm_get_blocking(pio, sm);
         for (int i = 31; i >= 0; --i) {
-            printf("%"PRIu32, v >> i & 1);
+            printf("%"PRIu32, value >> i & 1);
+        }
+        printf("                ");
+        uint16_t n[3];
+        uint16_t p;
+        if (value != 0xFFFFF) {
+            uint32_t gcr = (value ^ (value >> 1));
+            n[0] = gcr_decode(gcr & 0x1F);
+            n[1] = gcr_decode((gcr >> 5) & 0x1F);
+            n[2] = gcr_decode((gcr >> 10) & 0x1F);
+            p = gcr_decode((gcr >> 15) & 0x1F);
+            if ((p & 0x1) == 1) {
+                uint16_t rpm = (n[0] | (n[1] << 4) | (n[2] << 8) | ((p & 0x1) << 12));
+                // for (int i = 15; i >= 0; --i) {
+                //     printf("%"PRIu16, rpm >> i & 1);
+                // }
+                printf("%d", rpm);
+            }
         }
         printf("\n");
         sleep_us(700);
