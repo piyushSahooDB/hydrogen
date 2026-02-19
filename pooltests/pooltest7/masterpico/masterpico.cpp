928b9a9032c3dbaa189b72bb55f6752a4bb83737
ChaturVasireddy <chaturvasireddy@gmail.com>
2026-02-19 17:25:34 +0530

    added timer interrupt for imu update and control loop for 100ms

--- pooltests/pooltest7/masterpico/masterpico.cpp
+++ pooltests/pooltest7/masterpico/masterpico.cpp
@@ -15,14 +15,14 @@
 #include "imu.hpp"
 #include "control.hpp"
 
-// volatile bool control_flag = false;
-// struct repeating_timer control_timer;
+volatile bool control_flag = false;
+struct repeating_timer control_timer;
 
-// bool control_timer_cb(struct repeating_timer* t)
-// {
-//     control_flag = true;   // ISR only sets flag
-//     return true;
-// }
+bool control_timer_cb(struct repeating_timer* t)
+{
+    control_flag = true;
+    return true;
+}
 
 State state;
 Throttle throttle = { 0, 0, 0, 0, 0, 0 };
@@ -58,22 +58,35 @@ void arm_thrusters() {
 
 int main(void) {
 
+    // gpio_init(15);
+    // gpio_set_dir(15, GPIO_OUT);
+    // gpio_put(15, 0);
+
     stdio_init_all();
 
-    sleep_ms(1000);
+    sleep_ms(5000);
     printf("program initiating\n");
 
     imu::init();
     allthrusters_init();
     arm_thrusters();
 
-
     printf("program initialised\n");
+
+    add_repeating_timer_ms(
+        -100,                //100ms control loop
+        control_timer_cb,
+        NULL,
+        &control_timer
+    );
 
     while (1) {
 
-        imu::update();
-        control::update();
+        if (control_flag) {
+            control_flag = false;
+            imu::update();
+            control::update();
+        }
 
         // printf("%d      %d      %d\n", throttle.VB, throttle.VR, throttle.VL);
 
@@ -88,7 +101,7 @@ int main(void) {
         }
         printf("\n");
 
-        sleep_ms(70);
+        sleep_us(700);
     }
 
 }
